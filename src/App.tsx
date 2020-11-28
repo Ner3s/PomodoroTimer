976f5f8eb54daa2a4ab9ba9d2eb72f4f7f9c399c
Alan Neres <alanneres10@hotmail.com>
2020-11-28 20:21:03 -0300

    Feat: create a new hook for setInterval delay

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import PomodoroTimer from './components/pomodoro-timer';
 
 function App() {
-  return <div>Olá mundo</div>;
+  return (
+    <div>
+      <PomodoroTimer defaultPomodoroTime={1500} />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/pomodoro-timer.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { useInterval } from '../hooks/use-interval';
+
+interface Props {
+  defaultPomodoroTime: number;
+}
+
+function PomodoroTimer(props: Props) {
+  const [mainTime, setMainTime] = React.useState(props.defaultPomodoroTime);
+
+  useInterval(() => {
+    setMainTime(mainTime - 1);
+  }, 1000);
+
+  return <div>Olá mundo{mainTime}</div>;
+}
+
+export default PomodoroTimer;
